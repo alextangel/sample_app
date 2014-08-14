b6a2ebf257477f0fa3dd3fb4c48d2afc4190f713
Alex Tangel <weisse.taube93@gmail.com>
2014-08-14 06:29:16 +0000

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,25 +5,30 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
     
-    it "should have the right title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Home")
+    it "should have the base title" do
+      visit root_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+    
+    it "should not have a custom page title" do
+      visit root_path
+      expect(page).not_to have_title('| Home')
     end
   end
   
   describe "Help page" do
     
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     
     it "should have the right title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Help")
     end
     
@@ -32,12 +37,12 @@ describe "Static pages" do
   describe "About page" do
     
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     
     it "should have the right title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("About")
     end
   end
